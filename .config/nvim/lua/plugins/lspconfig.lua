9fd535eaea2ee4474b58e1e9974ce0ae15b4a0ce
rajshekhar26 <rjshekhar.official@gmail.com>
2021-08-22 16:53:48 +0530

    Add css and html language server setup

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -54,3 +54,14 @@ for _, lsp in ipairs(servers) do
     on_attach = on_attach
   }
 end
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+require'lspconfig'.html.setup {
+  capabilities = capabilities,
+}
+
+require'lspconfig'.cssls.setup {
+  capabilities = capabilities,
+}
